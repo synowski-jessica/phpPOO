b2967e39ffc27c51db6ef0cddbcd9b7f2f8be1d0
synowski jessica <148389622+synowski-jessica@users.noreply.github.com>
2024-11-05 09:41:12 +0100

    ajout accesseur, mutateur et constructeur à la class player

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-
 <?php
-
 
 class Encounter
 {
@@ -11,7 +9,7 @@ class Encounter
 
     public static function probabilityAgainst(Player $playerOne, Player $playerTwo): float
     {
-        return 1/(1+(10 ** (($playerTwo->level - $playerOne->level)/400)));
+        return 1/(1+(10 ** (($playerTwo->getLevel() - $playerOne->getLevel())/400)));
     }
 
     public static function setNewLevel(Player $playerOne, Player $playerTwo, int $playerOneResult): void
@@ -20,27 +18,40 @@ class Encounter
             trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', self::RESULT_POSSIBILITIES)));
         }
 
-        $playerOne->level += round(32 * ($playerOneResult - self::probabilityAgainst($playerOne, $playerTwo)));
+        $playerOne->setLevel(
+            $playerOne->getLevel() +
+            round(32 * ($playerOneResult - self::probabilityAgainst($playerOne, $playerTwo)))
+        );
     }
 }
 
 class Player
 {
-    public int $level;
+    private int $level;
+
+    public function __construct(int $level)
+    {
+        $this->level = $level;
+    }
+
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): void
+    {
+        $this->level = $level;
+    }
 }
 
-$greg = new Player;
-$jade = new Player;
-
-$greg->level = 400;
-$jade->level = 800;
-
-//$encounter = new Encounter;
+$greg = new Player(400);
+$jade = new Player(800);
 
 echo sprintf(
         'Greg à %.2f%% chance de gagner face a Jade',
         Encounter::probabilityAgainst($greg, $jade)*100
-    );
+    ).PHP_EOL;
 
 // Imaginons que greg l'emporte tout de même.
 Encounter::setNewLevel($greg, $jade, Encounter::RESULT_WINNER);
@@ -48,9 +59,8 @@ Encounter::setNewLevel($jade, $greg, Encounter::RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
-    $greg->level,
-    $jade->level
+    $greg->getLevel(),
+    $jade->getLevel()
 );
 
 exit(0);
-?>
